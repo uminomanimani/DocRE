8b12b15eed2f0dc3de1d4ab6875e9e441839267a
uminomanimani <ma_likai@icloud.com>
2023-10-19 18:05:00 +0800

    又是想死掉的一天

--- code/docre_data.py
+++ code/docre_data.py
@@ -1,162 +1,23 @@
-import numpy as np
-import json
-import os
-import torch
+import logging
+logging.getLogger("transformers").setLevel(logging.ERROR)
+
+from load_data import ReadDataset
 from torch.utils.data import Dataset
-from transformers import BertModel
-import time
-
-
-
-def load_data(prefix, data_path='./prepro_data'):
-    print("Reading training data...")
-
-    print('train', prefix)
-    # data_word是每个（3053）个page，每篇中的word通过word2id映射到token的列表，长度为max_length
-    data_word = np.load(os.path.join(data_path, prefix+'_word.npy'))
-    # data_pos是每个实体在vertexSet中对应的实体的序号，长度为这篇文档中max_length
-    data_pos = np.load(os.path.join(data_path, prefix+'_pos.npy'))
-    # data_ner是代表每个word的实体类型通过ner2id映射的列表，长度为512/1024
-    data_ner = np.load(os.path.join(data_path, prefix+'_ner.npy'))
-    # 每个单词的字母通过char2id映射到数字
-    data_char = np.load(os.path.join(data_path, prefix+'_char.npy'))
-    # 有几个字段 na_triple代表没有关系的实体三元组，labels代表头尾实体和关系，vertexSet和json里相同，title是page的标题，Ls代表每个句子在page中的起始位置
-    file = json.load(open(os.path.join(data_path, prefix+'.json')))
-
-    print("Finish reading")
-
-    return data_word, data_pos, data_ner, data_char, file
-        
+from transformers import AutoTokenizer
 
 class DocREDataset(Dataset):
-    def __init__(self, prefix : str, data_path : str):
-        t1 = time.time()
-        print('start')
-        self.data_word, self.data_pos, self.data_ner, self.data_char, self.file = load_data(prefix=prefix, data_path=data_path)
+    def __init__(self, file : str, tokenizer):
+        self.tokenizer = tokenizer
+        self.readDataset = ReadDataset('docred', self.tokenizer, max_seq_Length=1024)
 
-        l = len(self.data_word)
-        assert(len(self.data_word) == len(self.data_pos) == len(self.data_ner) == len(self.file))
-
-        self.data = []
-
-        maxNumEntity = 0
-        
-        for i in range(l):
-            words = self.data_word[i] #这篇文档中单词映射到token的列表
-            lenWords = self.file[i]['Ls'][-1]
-            labels = self.file[i]['labels'] #这篇文档中的标签
-            naTriple = self.file[i]['na_triple']
-            numEntity = len(self.file[i]['vertexSet']) #这篇文档中实体的数量
-            if numEntity > maxNumEntity:
-                maxNumEntity = numEntity
-            segmentID = []
-            for j in range(1, len(self.file[i]['Ls'])):
-                segmentID = segmentID + [0 if (j - 1) % 2 == 0 else 1] * (self.file[i]['Ls'][j] - self.file[i]['Ls'][j - 1])
-            segmentID = segmentID + (1024 - len(segmentID)) * [0]
-            segmentID = np.array(segmentID).astype(np.int32)
-
-            pageData = []
-            triples = []
-
-            for label in labels:
-                triple = [label['h'], label['t'], label['r']]
-                triples.append(triple)
-                pass
-
-            for x in range(numEntity):
-                for y in range(numEntity):
-                    if x != y:
-                        if [x, y] not in naTriple:
-                            for triple in triples:
-                                if triple[0] == x and triple[1] == y:
-                                    relation = triple[2]
-                        else:
-                            relation = None
-
-                        entityPos = []
-                        headEntity = self.file[i]['vertexSet'][x]
-                        tailEntity = self.file[i]['vertexSet'][y]
-                        for headEntityMention in headEntity:
-                            # sent_id = headEntityMention['sent_id']
-                            start = headEntityMention['pos'][0]
-                            end = headEntityMention['pos'][1]
-                            entityPos.append([start, end, 'h'])
-                        for tailEntityMention in tailEntity:
-                            # sent_id = tailEntityMention['sent_id']
-                            start = tailEntityMention['pos'][0]
-                            end = tailEntityMention['pos'][1]
-                            entityPos.append([start, end, 't'])
-                        
-                        replacedWords, mask, replacedSegmentID, headMentionPos, tailMentionPos = self.replaceMention(words=words, entityPos=entityPos, segmentID=segmentID, lenWords=lenWords)
-                        replacedWords = torch.from_numpy(replacedWords)
-                        mask = torch.from_numpy(mask)
-                        replacedSegmentID = torch.from_numpy(replacedSegmentID)
-                        headMentionPos = torch.from_numpy(headMentionPos)
-                        tailMentionPos = torch.from_numpy(tailMentionPos)
-                        # 返回：插入了标记的文档，插入了标记后的段落ID，头实体提及的标记位置，尾实体提及的标记位置，头实体ID，尾实体ID，关系编号
-                        pageData.append([replacedWords, mask, replacedSegmentID, headMentionPos, tailMentionPos, x, y, relation])
-            
-            # pageData = pageData + (maxPairLength - len(pageData) - 1) * [[]] + [[len(pageData)]]          
-            self.data.append(pageData)
-            pass
-        t2 = time.time()
-        print(maxNumEntity)
-        print(f'finished after {t2-t1}')
-        pass
-
-    def replaceMention(self, words, entityPos, segmentID, lenWords):
-        sortedPos = sorted(entityPos, key=lambda x : x[0])
-        replacedSegmentID = np.array([]).astype(np.int32)
-        replacedWords = np.array([]).astype(np.int32)
-        headMentionPos = np.array([]).astype(np.int32)
-        tailMentionPos = np.array([]).astype(np.int32)
-        lastEnd = 0
-        for start, end, s in sortedPos:  #start、end都是在原文档中实体提及的开始和结束位置
-            insertPos = 0
-            if start < lastEnd:  # 表明出现了重叠实体提及
-                insertPos = len(replacedWords) - (lastEnd - start)     #要在哪里插入重叠提及的标记
-                replacedWords = np.concatenate((replacedWords[:insertPos], np.array([1008]).astype(np.int32), replacedWords[insertPos:]))
-                replacedSegmentID = np.concatenate((replacedSegmentID[:insertPos], np.array([segmentID[start]]).astype(np.int32), replacedSegmentID[insertPos:]))
-                # lastEnd = end
-            else:
-                replacedWords = np.concatenate((replacedWords, words[lastEnd:start]))
-                insertPos = len(replacedWords)
-                replacedWords = np.concatenate((replacedWords, np.array([1008]).astype(np.int32), words[start:end]))
-                replacedSegmentID = np.concatenate((replacedSegmentID, segmentID[lastEnd:start], np.array([segmentID[start]]).astype(np.int32), segmentID[start:end]))
-                lastEnd = end
-            lenWords = lenWords + 1  # 每插入一个标记，文档长度就会+1
-            if s == 'h':
-                headMentionPos = np.append(headMentionPos, insertPos)
-            else:
-                tailMentionPos = np.append(tailMentionPos, insertPos)
-        
-        replacedWords = np.concatenate((replacedWords, words[lastEnd:])).astype(np.int32)
-        replacedSegmentID = np.concatenate((replacedSegmentID, segmentID[lastEnd:])).astype(np.int32)
-        
-        if len(replacedWords) > 1024:
-            replacedWords = replacedWords[0:1024]
-            replacedSegmentID = replacedSegmentID[0:1024]
-        else:
-            replacedWords = np.pad(replacedWords, (0, 1024 - len(replacedWords)), mode='constant', constant_values=0)
-            replacedSegmentID = np.pad(replacedSegmentID,  (0, 1024 - len(replacedSegmentID)), mode='constant', constant_values=0)
-
-        ones = np.ones(lenWords, dtype=np.int32)
-        zeros = np.zeros(1024 - lenWords, dtype=np.int32)
-        mask = np.concatenate((ones, zeros))
-
-        return replacedWords, mask, replacedSegmentID, headMentionPos, tailMentionPos
+        self.features = self.readDataset.read(file_in=file)
     
     def __len__(self):
-        return len(self.data)
+        return len(self.features)
     
     def __getitem__(self, index):
-        return self.data[index]
-        
+        return self.features[index]
 
 if __name__ == '__main__':
-    d = DocREDataset('dev_train', './prepro_data', '../pretrained/')
-    x = d.__getitem__(114)
-    y = d.__getitem__(514)
-    z = d.__getitem__(1919)
-    a = d.__getitem__(810)
+    d = DocREDataset('../data/test.json')
     pass
--- code/model.py
+++ code/model.py
@@ -1,117 +1,150 @@
-from typing import Any
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from transformers import BertModel
 from docre_data import DocREDataset
-import math
+from transformers import AutoConfig, AutoModel, AutoTokenizer
+import torch.nn.functional as F
 
-# def encode(bert, replacedWords, replacedSegmentID, headMentionPos, tailMentionPos):
-#     lenPage = np.argwhere(replacedWords == 0)[0][0] if len(np.argwhere(replacedWords == 0)) > 0 else 1024
-#     mask = np.array([1] * lenPage + (1024 - lenPage) * [0]).astype(np.int32)
-#     # replacedWords = torch.from_numpy(replacedWords).unsqueeze(0)
-#     # replacedSegmentID = torch.from_numpy(replacedSegmentID).unsqueeze(0)
-#     mask = torch.from_numpy(mask).unsqueeze(0)
-#     # print(replacedWords.shape)
-#     # print(replacedSegmentID.shape)
-#     # print(mask.shape)
-
-#     hiddenStates = bert(input_ids=replacedWords, attention_mask=mask, token_type_ids=replacedSegmentID)[0]
-#     hiddenStates = torch.squeeze(input=hiddenStates, dim=0)
-#     headMentionPos = torch.from_numpy(headMentionPos)
-#     tailMentionPos = torch.from_numpy(tailMentionPos)
-#     headMentionRepresentition = hiddenStates[headMentionPos]
-#     tailMentionRepresentation = hiddenStates[tailMentionPos]
-#     headEntityRepresentation = torch.logsumexp(headMentionRepresentition, dim=0)
-#     tailEntityRepresentation = torch.logsumexp(tailMentionRepresentation, dim=0)
-#     return headEntityRepresentation, tailEntityRepresentation
-
-# 这些参数的的第一个维度都是batch_size
-# 数据的格式(batch_size = 16)：
-# [
-#     [[replacedPage0, mask, replacedSegmentID, headMentionPos, tailMentionPos, headEntityID, tailEntityID, relation], [...], [...], ...]
-#     [[replacedPage1...], [...], ...]
-#     [...]
-#     ...
-#     [[replacedPage15...], [...], ...]
-#]
-
+# 每条包含以下几个部分：
+# input_ids:单词转为token的列表，长度和文章长度相同
+# entity_pos:实体的提及的开始和结束的位置
+# labels: 实体对之间的关系标签的独热编码，数量为n * (n - 1)，每个元素长度为关系的数量（97）
+# hts : 两两组合的实体对的集合，长度为n * (n - 1)，n为实体的数量
 def collate_fn(batch):
-    label = []
-    for items in batch:  #len(items)是实体对的数量. label map的数量和batch_size的大小相同
-        numEntity = math.ceil(math.sqrt(len(items)))
-        labelMap = torch.zeros(size=(numEntity, numEntity), dtype=torch.int32)
-        for item in items:
-            if item[7] != None:
-                labelMap[item[5]][item[6]] = item[7]
-        label.append(labelMap)
-    return batch, label
+    max_len = max([len(f["input_ids"]) for f in batch])
+    input_ids = [f["input_ids"] + [0] * (max_len - len(f["input_ids"])) for f in batch]
+    input_mask = [[1.0] * len(f["input_ids"]) + [0.0] * (max_len - len(f["input_ids"])) for f in batch]
+    input_ids = torch.tensor(input_ids, dtype=torch.long)
+    input_mask = torch.tensor(input_mask, dtype=torch.float)
+    entity_pos = [f["entity_pos"] for f in batch]
+
+    labels = [f["labels"] for f in batch]
+    hts = [f["hts"] for f in batch]
+    output = (input_ids, input_mask, labels, entity_pos, hts)
+    return output
 
 class Encode:
-    def __init__(self, bert):
+    def __init__(self, bert, config):
         self.bert = bert
+        self.config = config
         pass 
 
-    def __call__(self, batch):
-        featureMaps = []
-        for items in batch:
-            numEntity = math.ceil(math.sqrt(len(items)))
-            replacedWords = []
-            masks = []
-            segmentIDs = []
-            headMentionPos = []
-            tailMentionPos = []
-            headEntityID = []
-            tailEntityID = []
-            for item in items:
-                replacedWords.append(item[0])
-                masks.append(item[1])
-                segmentIDs.append(item[2])
-                headMentionPos.append(item[3])
-                tailMentionPos.append(item[4])
-                headEntityID.append(item[5])
-                tailEntityID.append(item[6])
-            # replacedWords = torch.tensor(replacedWords, dtype=torch.int32)
-            # masks = torch.tensor(masks, dtype=torch.int32)
-            # segmentIDs = torch.tensor(segmentIDs, dtype=torch.int32)
-            hiddenStates = self.bert(input_ids=replacedWords, attention_mask=masks, token_type_ids=segmentIDs)[0]
-            featureMap = torch.zeros(size=(numEntity, numEntity, 768 * 2))
-            for headMention, tailMention, headID, tailID, hiddenState in zip(headMentionPos, 
-                                                                             tailMentionPos, 
-                                                                             headEntityID, 
-                                                                             tailEntityID, 
-                                                                             hiddenStates):
-                headMentionRepresentation = hiddenState[headMention]
-                tailMentionRepresentation = hiddenState[tailMention]
-                headEntityRepresentation = torch.logsumexp(headMentionRepresentation, dim=0)
-                tailEntityRepresentation = torch.logsumexp(tailMentionRepresentation, dim=0)
-                
-        pass  
+    def process_long_input(self, model, input_ids, attention_mask):
+        # Split the input to 2 overlapping chunks. Now BERT can encode inputs of which the length are up to 1024.
+        start_tokens = [self.config.cls_token_id]
+        end_tokens = [self.config.sep_token_id]
+        n, c = input_ids.size()
+        start_tokens = torch.tensor(start_tokens).to(input_ids)
+        end_tokens = torch.tensor(end_tokens).to(input_ids)
+        len_start = start_tokens.size(0)
+        len_end = end_tokens.size(0)
+        if c <= 512:
+            output = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                output_attentions=True,
+            )
+            sequence_output = output[0]
+            attention = output[-1][-1]
+        else:
+            new_input_ids, new_attention_mask, num_seg = [], [], []
+            seq_len = attention_mask.sum(1).cpu().numpy().astype(np.int32).tolist()
+            for i, l_i in enumerate(seq_len):
+                if l_i <= 512:
+                    new_input_ids.append(input_ids[i, :512])
+                    new_attention_mask.append(attention_mask[i, :512])
+                    num_seg.append(1)
+                else:
+                    input_ids1 = torch.cat([input_ids[i, :512 - len_end], end_tokens], dim=-1)
+                    input_ids2 = torch.cat([start_tokens, input_ids[i, (l_i - 512 + len_start): l_i]], dim=-1)
+                    attention_mask1 = attention_mask[i, :512]
+                    attention_mask2 = attention_mask[i, (l_i - 512): l_i]
+                    new_input_ids.extend([input_ids1, input_ids2])
+                    new_attention_mask.extend([attention_mask1, attention_mask2])
+                    num_seg.append(2)
+            input_ids = torch.stack(new_input_ids, dim=0)
+            attention_mask = torch.stack(new_attention_mask, dim=0)
+            output = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                output_attentions=True,
+            )
+            sequence_output = output[0]
+            attention = output[-1][-1]
+            i = 0
+            new_output, new_attention = [], []
+            for (n_s, l_i) in zip(num_seg, seq_len):
+                if n_s == 1:
+                    output = F.pad(sequence_output[i], (0, 0, 0, c - 512))
+                    att = F.pad(attention[i], (0, c - 512, 0, c - 512))
+                    new_output.append(output)
+                    new_attention.append(att)
+                elif n_s == 2:
+                    output1 = sequence_output[i][:512 - len_end]
+                    mask1 = attention_mask[i][:512 - len_end]
+                    att1 = attention[i][:, :512 - len_end, :512 - len_end]
+                    output1 = F.pad(output1, (0, 0, 0, c - 512 + len_end))
+                    mask1 = F.pad(mask1, (0, c - 512 + len_end))
+                    att1 = F.pad(att1, (0, c - 512 + len_end, 0, c - 512 + len_end))
+
+                    output2 = sequence_output[i + 1][len_start:]
+                    mask2 = attention_mask[i + 1][len_start:]
+                    att2 = attention[i + 1][:, len_start:, len_start:]
+                    output2 = F.pad(output2, (0, 0, l_i - 512 + len_start, c - l_i))
+                    mask2 = F.pad(mask2, (l_i - 512 + len_start, c - l_i))
+                    att2 = F.pad(att2, [l_i - 512 + len_start, c - l_i, l_i - 512 + len_start, c - l_i])
+                    mask = mask1 + mask2 + 1e-10
+                    output = (output1 + output2) / mask.unsqueeze(-1)
+                    att = (att1 + att2)
+                    att = att / (att.sum(-1, keepdim=True) + 1e-10)
+                    new_output.append(output)
+                    new_attention.append(att)
+                i += n_s
+            sequence_output = torch.stack(new_output, dim=0)
+            attention = torch.stack(new_attention, dim=0)
+        return sequence_output, attention
+
+
+    def __call__(self, input_ids, masks):
+        sequence_output, attention = self.process_long_input(self.bert, input_ids=input_ids, attention_mask=masks)
+        return sequence_output, attention 
 
 
 class DocREModel(nn.Module):
-    def __init__(self, bertPath : str) -> None:
+    def __init__(self, bert, config) -> None:
         super().__init__()
-        self.bert = BertModel.from_pretrained(bertPath)
+        self.bert = bert
+        self.config = config
 
         for param in self.bert.parameters():
             param.requires_grad = False
         
-        self.encode = Encode(bert=self.bert)
+        self.encode = Encode(bert=self.bert, config=config)
 
     
     def forward(self, batch):
-        featureMaps = self.encode(batch=batch)
+        sequence_output, attention = self.encode(input_ids=batch[0], masks=batch[1])
         return None
-        pass
 
         
 
 if __name__ == "__main__":
-    model = DocREModel('../pretrained')
-    dataset = DocREDataset('dev_test', './prepro_data')
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
+
+    config = AutoConfig.from_pretrained('bert-base-cased', num_labels=97)
+    config.cls_token_id = tokenizer.cls_token_id
+    config.sep_token_id = tokenizer.sep_token_id
+    
+    bert = AutoModel.from_pretrained(
+        'bert-base-cased',
+        from_tf=False,
+        config=config,
+    )
+
+    dataset = DocREDataset('../data/test.json', tokenizer=tokenizer)
     dataloader = DataLoader(dataset=dataset, batch_size=4, collate_fn=collate_fn, shuffle=True)
-    for data, label in dataloader:
-        x = model(data)
+    model = DocREModel(bert, config)
+    for batch in dataloader:
+        x = model(batch)
         pass
